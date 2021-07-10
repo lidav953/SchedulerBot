3e1fd0af0f68d975f80e53bba4113577e126e515
David Li <lidav953@gmail.com>
2021-07-10 15:21:23 -0700

    added SchedulerBot class

---
+++ bot.py
@@ -0,0 +1,14 @@
+from secret import *
+import discord
+import requests
+import json
+import bot
+
+class SchedulerBot(discord.Client):
+    def __init__(self, AUTH_TOKEN):
+        super(SchedulerBot, self).__init__()
+        self.auth_token = AUTH_TOKEN
+    
+    def run(self):
+        print('running')
+        super(SchedulerBot, self).run(self.auth_token)
--- main.py
+++ main.py
@@ -2,26 +2,8 @@ from secret import *
 import discord
 import requests
 import json
+import bot
 
-client = discord.Client()
-
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
-
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('$inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
-
-client.run(AUTH_TOKEN)
+if __name__ == "__main__":
+    bot = bot.SchedulerBot(AUTH_TOKEN)
+    bot.run()
